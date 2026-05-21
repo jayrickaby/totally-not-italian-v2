b3fa5c824614d39a1296101923591ceab553b22c
Jay <jayrickaby@pm.me>
2026-05-21 23:48:44 +0100

    Added jump sfx, made jump shorter

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var snd_jump: AudioStreamPlayer2D = $snd_jump
 
 const SPEED = 10.0
-const JUMP_VELOCITY = -850.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -26,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and  is_on_floor():
 		velocity.y = JUMP_VELOCITY	
+		snd_jump.play()
 
 	if direction:
 		velocity.x = direction * SPEED
