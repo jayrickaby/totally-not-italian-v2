1cd283760b26369d5e75dd8a1468b6a28f04da63
Jay <jayrickaby@pm.me>
2026-05-21 23:26:29 +0100

    Adjusted physics: gravity, player speed and player jump height. Added animations and sprite flipping when direction changes. Moved player collision shape to actual player object.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 10.0
+const JUMP_VELOCITY = -850.0
 
 
 func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+	
+	# Add animation
+	if not is_on_floor():
+		animated_sprite_2d.animation = "jump"
+	elif velocity.x != 0:
+		animated_sprite_2d.animation = "walk"
+		animated_sprite_2d.flip_h = (direction == -1)
+	else:
+		animated_sprite_2d.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and  is_on_floor():
 		velocity.y = JUMP_VELOCITY	
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
