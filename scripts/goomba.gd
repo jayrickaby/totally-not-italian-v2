a9c8e82b52817f57ed90b9083c2d9bd868717851
Jay Rickaby <jayrickaby@pm.me>
2026-05-24 00:04:31 +0100

    Added death animation to goomba!

--- scripts/goomba.gd
+++ scripts/goomba.gd
@@ -6,10 +6,13 @@ extends CharacterBody2D
 signal player_damaged
 
 const SPEED = 20.0
-var direction := -1
+var direction = -1
+var alive = true;
 
-
-func _physics_process(delta: float) -> void:		
+func _physics_process(delta: float) -> void:	
+	if !alive:
+		return
+			
 	if velocity.x > 1 or velocity.x < -1:
 		animated_sprite_2d.animation = "walk"
 		animated_sprite_2d.flip_h = (direction == -1)
@@ -30,17 +33,21 @@ func _physics_process(delta: float) -> void:
 		direction *= -1
 
 func _on_hurt_area_entered(body: Node2D) -> void:
-	if (body.name == "Player" && body.alive):
+	if (body.name == "Player" && body.alive && alive):
 		emit_signal("player_damaged", body)
 
 
 func _on_stomp_area_entered(body: Node2D) -> void:
-	if (body.name == "Player" && body.alive && !body.is_on_floor()):
+	if (body.name == "Player" && body.alive && !body.is_on_floor() && alive):
 		stomp()
 		
 func stomp() -> void:
-	call_deferred("die")
+	alive = false;
+	animated_sprite_2d.animation = "stomped"
+
+func _on_animation_looped() -> void:
+	if (animated_sprite_2d.animation == "stomped"):
+		call_deferred("die")
 	
 func die() -> void:
 	queue_free()
-	
