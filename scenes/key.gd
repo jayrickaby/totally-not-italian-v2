cec28afe31ecb5be5965e9ba8c16daa5be744160
Jay Rickaby <jayrickaby@pm.me>
2026-05-23 23:28:48 +0100

    Adjusted some key logic and placement

--- scenes/key.gd
+++ scenes/key.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const BOUNCE_VELOCITY = -10000
+const BOUNCE_VELOCITY = -200
 var currentBounceVelocity = 0
 var direction = 0
 
@@ -9,14 +9,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	move_and_slide()
 	# Bounce Logic!!! RAGH
 	if not is_on_floor() && currentBounceVelocity == 0:
 		currentBounceVelocity = BOUNCE_VELOCITY
 	elif is_on_floor() && currentBounceVelocity < -25:
-		velocity.y += currentBounceVelocity * delta
+		velocity.y += currentBounceVelocity
 		currentBounceVelocity /= 3
-	elif is_on_floor() && currentBounceVelocity > -25:
+	elif is_on_floor() && currentBounceVelocity >= -25:
 		currentBounceVelocity = 0	
-
-
-	move_and_slide()
