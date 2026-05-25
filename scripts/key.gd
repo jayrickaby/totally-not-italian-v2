cd50ef17b4a6f6fcc10bb129ecb385fc67d5d544
Jay Rickaby <jayrickaby@pm.me>
2026-05-25 22:23:47 +0100

    Moved location of door script and added bounce component which key uses

--- scripts/key.gd
+++ scripts/key.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-const BOUNCE_VELOCITY = -200
-var currentBounceVelocity = 0
 var direction = 0
+@onready var bounce_component: Node = $BounceComponent
 
 @export var id: String = ""
 
@@ -29,14 +28,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	move_and_slide()
-	# Bounce Logic!!! RAGH
-	if not is_on_floor() && currentBounceVelocity == 0:
-		currentBounceVelocity = BOUNCE_VELOCITY
-	elif is_on_floor() && currentBounceVelocity < -25:
-		velocity.y += currentBounceVelocity
-		currentBounceVelocity /= 3
-	elif is_on_floor() && currentBounceVelocity >= -25:
-		currentBounceVelocity = 0	
+	bounce_component.apply(self)
+	
+
 
 func _on_collect_area_entered(body: Node2D) -> void:
 	if (body.name == "Player" && body.alive):
---
+++ scripts/shared/bounce_component.gd
@@ -0,0 +1,18 @@
+extends Node
+@export var BOUNCE_VELOCITY: float = 200
+@export var BOUNCE_CUTOFF: float = 25
+@export var BOUNCE_FALLOFF: float = 4
+var currentBounceVelocity = 0
+
+func apply(parent: CharacterBody2D) -> void:
+	if parent.is_on_floor() && currentBounceVelocity > BOUNCE_CUTOFF:
+		parent.velocity.y -= currentBounceVelocity
+		currentBounceVelocity /= BOUNCE_FALLOFF
+		return
+	
+	if currentBounceVelocity == 0:
+		currentBounceVelocity += BOUNCE_VELOCITY
+
+
+		
+	currentBounceVelocity = 0
