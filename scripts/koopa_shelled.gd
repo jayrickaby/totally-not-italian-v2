677b0273d05822acc3283aed464f97c2507197de
Jay <jayrickaby@pm.me>
2026-05-26 11:58:56 +0100

    Koopa now uses stomp component

--- scripts/koopa_shelled.gd
+++ scripts/koopa_shelled.gd
@@ -6,12 +6,24 @@ extends CharacterBody2D
 
 signal damage_to_player	
 
+var alive = true
+
 func _ready() -> void:
 	basic_move_component.direction_changed.connect(_change_direction)
-	stompable_component.stomped_by_player
+	stompable_component.stomped_by_player.connect(_stomp)
 
 func _physics_process(delta: float) -> void:
+	if !alive:
+		return
+		
 	basic_move_component.tick(self, delta)
 
 func _change_direction(direction: int) -> void:
 	animated_sprite_2d.flip_h = (direction == -1)
+
+func _stomp() -> void:
+	alive = false
+	call_deferred("die")
+
+func die() -> void:
+	queue_free()
