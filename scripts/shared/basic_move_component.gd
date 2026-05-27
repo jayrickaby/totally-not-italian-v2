2ce301e8f4541b1186c981a0d77723e672020341
Jay <jayrickaby@pm.me>
2026-05-27 09:39:46 +0100

    Added "None" direction to basic move component along with a "Set Direction" member function

--- scripts/shared/basic_move_component.gd
+++ scripts/shared/basic_move_component.gd
@@ -1,9 +1,9 @@
 class_name BasicMoveComponent
 extends Node
 
-enum directions {LEFT, RIGHT}
+enum Directions {LEFT, RIGHT, NONE}
 
-@export var initialDirection: directions = directions.RIGHT
+@export var initialDirection: Directions = Directions.RIGHT
 @export var flipOnWall: bool = 0
 @export var gravityScale: float = 1
 @export var speed: float = 100
@@ -12,10 +12,7 @@ var direction: int
 signal direction_changed
 
 func _ready() -> void:
-	if initialDirection == directions.LEFT:
-		direction = -1
-	else:
-		direction = 1
+	setDirection(initialDirection)
 
 func tick(parent: CharacterBody2D, delta: float) -> void:
 	if not parent.is_on_floor():
@@ -24,11 +21,19 @@ func tick(parent: CharacterBody2D, delta: float) -> void:
 	if direction:
 		parent.velocity.x = direction * speed
 	else:
-		parent.velocity.x = parent.move_toward(parent.velocity.x, 0, speed)
+		parent.velocity.x = move_toward(parent.velocity.x, 0, speed)
 
 	parent.move_and_slide()
 	
 	if parent.is_on_wall() && flipOnWall:
 		direction *= -1
 		emit_signal("direction_changed", direction)
+		
+func setDirection(newDirection: Directions) -> void:
+	if newDirection == Directions.LEFT:
+		direction = -1
+	elif newDirection == Directions.RIGHT:
+		direction = 1
+	else:
+		direction = 0
 	
