4dd509543f7ca40855c19ee6802dc0923c4a1c06
Jay <jayrickaby@pm.me>
2026-05-27 12:17:28 +0100

    Added Koopa Shell extra collision areas and some logic function

--- scripts/koopa_shell.gd
+++ scripts/koopa_shell.gd
@@ -1,7 +1,24 @@
 extends CharacterBody2D
+
 @onready var basic_move_component: BasicMoveComponent = $BasicMoveComponent
+@export var initiallySpinning: BasicMoveComponent.Directions = BasicMoveComponent.Directions.NONE
+var isSpinning: bool = false
 
 signal damage_to_player
 
+func _ready() -> void:
+	basic_move_component.setDirection(initiallySpinning)
+	if initiallySpinning != BasicMoveComponent.Directions.NONE:
+		isSpinning = true
+ 
 func _physics_process(delta: float) -> void:
 	basic_move_component.tick(self, delta)
+
+func _on_hurt_area_entered(body: Node2D) -> void:
+	if body.name == "Player" and body.alive and isSpinning:
+		damage_to_player.emit(body)
+		
+		
+func _on_interaction_area_entered(body: Node2D) -> void:
+	if !(body.name == "Player" and body.alive) or isSpinning:
+		return
