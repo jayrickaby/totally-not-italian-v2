2a111425687567ac57b6440700d16e243a207907
Jay <jayrickaby@pm.me>
2026-05-27 09:55:30 +0100

    Added flipDirection function, have the signals emitted in both direction-modifying functions and adjusted flow of logic so that X and Y direction code are seperated

--- scripts/shared/basic_move_component.gd
+++ scripts/shared/basic_move_component.gd
@@ -19,19 +19,26 @@ func _ready() -> void:
 	setDirection(initialDirection)
 
 func tick(parent: CharacterBody2D, delta: float) -> void:
-	if not parent.is_on_floor():
-		parent.velocity += parent.get_gravity() * gravityScale * delta
+	# X DIRECTION
+	if parent.is_on_wall() && flipOnWall:
+		flipDirection()
 
 	if direction:
 		parent.velocity.x = direction * speed
 	else:
 		parent.velocity.x = move_toward(parent.velocity.x, 0, speed)
 
+	# Y DIRECTION
+	if not parent.is_on_floor():
+		parent.velocity += parent.get_gravity() * gravityScale * delta
+
 	parent.move_and_slide()
-	
-	if parent.is_on_wall() && flipOnWall:
-		direction *= -1
-		direction_changed.emit(direction)
+		
 		
 func setDirection(newDirection: Directions) -> void:
 	direction = newDirection
+	direction_changed.emit(direction)
+	
+func flipDirection() -> void:
+	direction *= -1
+	direction_changed.emit(direction)
