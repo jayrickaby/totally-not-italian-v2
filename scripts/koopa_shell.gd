77515bab63e7db984d26fc4e3388a1ee17d5caff
Jay <jayrickaby@pm.me>
2026-05-29 23:32:01 +0100

    Ditto. actual code. because godot git commit thing is stupid

--- scripts/koopa_shell.gd
+++ scripts/koopa_shell.gd
@@ -11,8 +11,8 @@ var isSpinning: bool = false
 
 func _ready() -> void:
 	basic_move_component.setDirection(initiallySpinning)
-	kickable_component.kicked.connect(_kick)
-	stompable_component.stomped_by_player.connect(_stomp)
+	#kickable_component.kicked.connect(_kick)
+	#stompable_component.stomped_by_player.connect(_stomp)
 	
 	if initiallySpinning != Directions.NONE:
 		isSpinning = true
@@ -29,17 +29,14 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.animation = "idle"
 		
 func _kick(kickedFrom: Directions) -> void:
-	if !alive:
+	if !alive or isSpinning:
 		return
-	if isSpinning:
-		return
-	
 	
 	if (kickedFrom == Directions.RIGHT):
 		_spin(Directions.LEFT)
 	else:
 		_spin(Directions.RIGHT)
-		
+				
 func _spin(spinDirection: Directions) -> void:
 	basic_move_component.setDirection(spinDirection)
 	if spinDirection != Directions.NONE:
@@ -47,11 +44,34 @@ func _spin(spinDirection: Directions) -> void:
 	else:
 		isSpinning = false
 	
-func _stomp(player: Node2D) -> void:
+func _stomp(player: Node2D) -> void:	
 	if !alive:
 		return
+
 		
 	if isSpinning:
 		_spin(Directions.NONE)
 		
 	player.initiateJump()
+	
+
+
+func _on_kick_area_body_entered(body: Node2D) -> void:
+	if body.name != "Player" or body.alive == false:
+		return
+	
+	var kickDir: Directions = Directions.NONE
+	
+	if body.global_position.x < global_position.x:
+		kickDir = Directions.RIGHT
+	elif body.global_position.x > global_position.x:
+		kickDir = Directions.LEFT
+	
+	if isSpinning and !body.is_on_floor():
+		_stomp(body)
+	elif !isSpinning and body.is_on_floor():
+		_kick(kickDir)
+	elif !isSpinning and !body.is_on_floor():
+		_stomp(body)
+		_kick(kickDir)
+	
