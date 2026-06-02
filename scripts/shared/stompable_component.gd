0dedb18ecd2dc4d5bf5a0224ab63d3ea6fae3bfd
Jay Rickaby <jayrickaby@pm.me>
2026-06-02 21:36:49 +0100

    Added TODO

--- scripts/shared/stompable_component.gd
+++ scripts/shared/stompable_component.gd
@@ -6,6 +6,7 @@ extends Node
 signal stomped_by_player(player: Node2D)
 signal player_stopped_stomping(player: Node2D)
 
+# TODO Connect with health component and work in harmony
 func _ready() -> void:
 	if stompArea:
 		stompArea.body_entered.connect(_on_stomp_area_entered)
