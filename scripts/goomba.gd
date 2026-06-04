a917a648340c08108dbae3951f53aff228dce5cc
Jay Rickaby <jayrickaby@pm.me>
2026-06-04 21:26:00 +0100

    Goomba no longer does damage when dying

--- scripts/goomba.gd
+++ scripts/goomba.gd
@@ -1,11 +1,11 @@
 extends EnemyBase
 
 @onready var basic_move_component: BasicMoveComponent = $BasicMoveComponent
+@onready var damage_component: DamageComponent = $DamageComponent
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var stompable_component: StompableComponent = $StompableComponent
 
 const HealthStates = HealthComponent.HealthStates
-var deathWaitForAnimation: bool = false
 
 func _ready() -> void:	
 	basic_move_component.direction_changed.connect(_change_direction)
@@ -30,9 +30,12 @@ func _stomp(player: Node2D) -> void:
 	if health_component.state != HealthStates.Alive:
 		return
 	
-	deathWaitForAnimation = true
 	animated_sprite_2d.animation = "stomped"
 	player.initiateJump()
+	_startDying()
+	
+func _startDying() -> void:
+	damage_component.enabled = false
 	health_component.die()
 	
 func _on_animation_finished() -> void:
